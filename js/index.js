d94a8fa9fb548d8c710da6b101a1ab9ec0172492
William Gotterer <william.gotterer@gmail.com>
2021-09-28 11:55:23 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -12,24 +12,23 @@ document.addEventListener("DOMContentLoaded", function() {
 function fetchBooks() {
     fetch("http://localhost:3000/books")
     .then((resp) => resp.json())
-    .then((books) => books.forEach(book => renderBooks(book)))
-}
+    .then((json) => renderBooks(json))
   
-    function renderBooks(book){
-        console.log(book);
-        const ul = document.querySelector("#list");
-        
+}
+    function renderBooks(books){
+        console.log(books);
+        const ul = document.querySelector("ul#list");
+        books.forEach(book => {
             const li = document.createElement("li");
             li.innerHTML = book.title
-            ul.append(li)
-            li.addEventListener("click", () => display(book))
-        
+            ul.appendChild(li)
+            li.addEventListener("click", ( )=> display(book))
+        })
     }
   
-
+  
   
   function display(book){
-      console.log(book)
       const showPanel = document.querySelector("div#show-panel")
 
       const author = document.createElement("p")
@@ -47,16 +46,35 @@ function fetchBooks() {
       const title = document.createElement("p")
       title.className = "title"
       title.innerHTML = book.title
-      console.log(title)
 
-    //   const userList = document.createElement("li")
-    //   userList.className = "users"
-    //   userList.innerHTML = book.users.forEach(person => person) 
 
-      const bookUsers = book.filter(book => console.log(book))
-      console.log(bookUsers)
+      const userList = document.createElement("ul")
+      userList.className = "users"
+      
+      book.users.forEach(person => { 
 
-      showPanel.append(author)
+        const innerList = document.createElement("li");
+        userList.append(innerList)
+        innerList.innerHTML = person.username
+      })
+
+      const bttn = document.createElement("button")
+      bttn.id = "button_add"
+      bttn.innerHTML = "LIKE"
+
+      bttn.addEventListener("click",() =>{
+
+    //     fetch("http://localhost:3000/books/:id"),{
+           
+    //     .then(response => response.json()),
+    //     .then(json => console.log(json)),
+        
+      })
+
+
+
+
+      showPanel.replaceChildren(img, title, subtitle, author, description, userList, bttn)
 
   }
 
@@ -70,24 +88,3 @@ function fetchBooks() {
 
 
 
-
-//   function renderBooks(books) { 
-//     console.log(books)
-//     const main = document.querySelector('main');
-//     books.forEach(book => {
-//       const h2 = document.createElement('h2');
-//       h2.innerHTML = book.name;
-//       main.appendChild(h2)
-//       const link = document.createElement("a");
-//       link.href = book.url
-//       link.innerHTML = "book link"
-//       main.appendChild(link)
-      
-//     });
-  
-//   }
-  
-//   document.addEventListener('DOMContentLoaded', function() {
-//     fetchBooks();
-//   });
-  
